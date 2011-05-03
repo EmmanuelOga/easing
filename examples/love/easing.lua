5a8075578cf364a5bcc9787da47584774656979c
Emmanuel Oga <EmmanuelOga@gmail.com>
2011-05-03 19:28:28 -0300

    updated love example

--- examples/love/easing.lua
+++ examples/love/easing.lua
@@ -4,6 +4,25 @@
 -- and http://code.google.com/p/tweener/ (jstweener javascript version)
 --
 
+--[[
+Disclaimer for Robert Penner's Easing Equations license:
+
+TERMS OF USE - EASING EQUATIONS
+
+Open source under the BSD License.
+
+Copyright © 2001 Robert Penner
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
+
 -- For all easing functions:
 -- t = time
 -- b = begin
@@ -38,6 +57,14 @@ local function inOutQuad(t, b, c, d)
     return c / 2 * pow(t, 2) + b
   else
     return -c / 2 * ((t - 1) * (t - 3) - 1) + b
+  end
+end
+
+local function outInQuad(t, b, c, d)
+  if t < d / 2 then
+    return outQuad (t * 2, b, c / 2, d)
+  else
+    return inQuad((t * 2) - d, b + c / 2, c / 2, d)
   end
 end
 
@@ -366,6 +393,7 @@ return {
   inQuad = inQuad,
   outQuad = outQuad,
   inOutQuad = inOutQuad,
+  outInQuad = outInQuad,
   inCubic  = inCubic ,
   outCubic = outCubic,
   inOutCubic = inOutCubic,
