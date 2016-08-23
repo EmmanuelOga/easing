3b9ca894d003cbc70db53486c628eba3fdaaa43c
Narrev <NarrevRBX@gmail.com>
2016-08-22 20:11:06 -0500

    Optimizing

--- lib/easing.lua
+++ lib/easing.lua
@@ -29,11 +29,11 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 -- c = change == ending - beginning
 -- d = duration (total time)
 
-local pow = math.pow
 local sin = math.sin
 local cos = math.cos
 local pi = math.pi
-local sqrt = math.sqrt
+local _2pi = 2 * pi
+local _halfpi = .5 * pi
 local abs = math.abs
 local asin  = math.asin
 
@@ -43,7 +43,7 @@ end
 
 local function inQuad(t, b, c, d)
   t = t / d
-  return c * pow(t, 2) + b
+  return c * t ^ 2 + b
 end
 
 local function outQuad(t, b, c, d)
@@ -54,121 +54,121 @@ end
 local function inOutQuad(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return c / 2 * pow(t, 2) + b
+    return c * .5 * t^2 + b
   else
-    return -c / 2 * ((t - 1) * (t - 3) - 1) + b
+    return -c * .5 * ((t - 1) * (t - 3) - 1) + b
   end
 end
 
 local function outInQuad(t, b, c, d)
-  if t < d / 2 then
-    return outQuad (t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outQuad (t * 2, b, c * .5, d)
   else
-    return inQuad((t * 2) - d, b + c / 2, c / 2, d)
+    return inQuad((t * 2) - d, b + c * .5, c * .5, d)
   end
 end
 
 local function inCubic (t, b, c, d)
   t = t / d
-  return c * pow(t, 3) + b
+  return c * t ^ 3 + b
 end
 
 local function outCubic(t, b, c, d)
   t = t / d - 1
-  return c * (pow(t, 3) + 1) + b
+  return c * (t ^ 3 + 1) + b
 end
 
 local function inOutCubic(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return c / 2 * t * t * t + b
+    return c * .5 * t * t * t + b
   else
     t = t - 2
-    return c / 2 * (t * t * t + 2) + b
+    return c * .5 * (t * t * t + 2) + b
   end
 end
 
 local function outInCubic(t, b, c, d)
-  if t < d / 2 then
-    return outCubic(t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outCubic(t * 2, b, c * .5, d)
   else
-    return inCubic((t * 2) - d, b + c / 2, c / 2, d)
+    return inCubic((t * 2) - d, b + c * .5, c * .5, d)
   end
 end
 
 local function inQuart(t, b, c, d)
   t = t / d
-  return c * pow(t, 4) + b
+  return c * t ^ 4 + b
 end
 
 local function outQuart(t, b, c, d)
   t = t / d - 1
-  return -c * (pow(t, 4) - 1) + b
+  return -c * (t ^ 4 - 1) + b
 end
 
 local function inOutQuart(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return c / 2 * pow(t, 4) + b
+    return c * .5 * t ^ 4 + b
   else
     t = t - 2
-    return -c / 2 * (pow(t, 4) - 2) + b
+    return -c * .5 * (t ^ 4 - 2) + b
   end
 end
 
 local function outInQuart(t, b, c, d)
-  if t < d / 2 then
-    return outQuart(t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outQuart(t * 2, b, c * .5, d)
   else
-    return inQuart((t * 2) - d, b + c / 2, c / 2, d)
+    return inQuart((t * 2) - d, b + c * .5, c * .5, d)
   end
 end
 
 local function inQuint(t, b, c, d)
   t = t / d
-  return c * pow(t, 5) + b
+  return c * t ^ 5 + b
 end
 
 local function outQuint(t, b, c, d)
   t = t / d - 1
-  return c * (pow(t, 5) + 1) + b
+  return c * (t ^ 5 + 1) + b
 end
 
 local function inOutQuint(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return c / 2 * pow(t, 5) + b
+    return c * .5 * t ^ 5 + b
   else
     t = t - 2
-    return c / 2 * (pow(t, 5) + 2) + b
+    return c * .5 * (t ^ 5 + 2) + b
   end
 end
 
 local function outInQuint(t, b, c, d)
-  if t < d / 2 then
-    return outQuint(t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outQuint(t * 2, b, c * .5, d)
   else
-    return inQuint((t * 2) - d, b + c / 2, c / 2, d)
+    return inQuint((t * 2) - d, b + c * .5, c * .5, d)
   end
 end
 
 local function inSine(t, b, c, d)
-  return -c * cos(t / d * (pi / 2)) + c + b
+  return -c * cos(t / d * _halfpi) + c + b
 end
 
 local function outSine(t, b, c, d)
-  return c * sin(t / d * (pi / 2)) + b
+  return c * sin(t / d * _halfpi) + b
 end
 
 local function inOutSine(t, b, c, d)
-  return -c / 2 * (cos(pi * t / d) - 1) + b
+  return -c * .5 * (cos(pi * t / d) - 1) + b
 end
 
 local function outInSine(t, b, c, d)
-  if t < d / 2 then
-    return outSine(t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outSine(t * 2, b, c * .5, d)
   else
-    return inSine((t * 2) -d, b + c / 2, c / 2, d)
+    return inSine((t * 2) -d, b + c * .5, c * .5, d)
   end
 end
 
@@ -176,7 +176,7 @@ local function inExpo(t, b, c, d)
   if t == 0 then
     return b
   else
-    return c * pow(2, 10 * (t / d - 1)) + b - c * 0.001
+    return c * 2 ^ (10 * (t / d - 1)) + b - c * 0.001
   end
 end
 
@@ -184,7 +184,7 @@ local function outExpo(t, b, c, d)
   if t == d then
     return b + c
   else
-    return c * 1.001 * (-pow(2, -10 * t / d) + 1) + b
+	return c * 1.001 * (1 - 2 ^ (-10 * t / d)) + b
   end
 end
 
@@ -193,46 +193,46 @@ local function inOutExpo(t, b, c, d)
   if t == d then return b + c end
   t = t / d * 2
   if t < 1 then
-    return c / 2 * pow(2, 10 * (t - 1)) + b - c * 0.0005
+    return c * .5 * 2 ^ (10 * (t - 1)) + b - c * 0.0005
   else
     t = t - 1
-    return c / 2 * 1.0005 * (-pow(2, -10 * t) + 2) + b
+    return c * .5 * 1.0005 * (2 - 2 ^ (-10 * t)) + b
   end
 end
 
 local function outInExpo(t, b, c, d)
-  if t < d / 2 then
-    return outExpo(t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outExpo(t * 2, b, c * .5, d)
   else
-    return inExpo((t * 2) - d, b + c / 2, c / 2, d)
+    return inExpo((t * 2) - d, b + c * .5, c * .5, d)
   end
 end
 
 local function inCirc(t, b, c, d)
   t = t / d
-  return(-c * (sqrt(1 - pow(t, 2)) - 1) + b)
+  return(-c * ((1 - t ^ 2) ^ .5 - 1) + b)
 end
 
 local function outCirc(t, b, c, d)
   t = t / d - 1
-  return(c * sqrt(1 - pow(t, 2)) + b)
+  return(c * (1 - t ^ 2) ^ .5 + b)
 end
 
 local function inOutCirc(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return -c / 2 * (sqrt(1 - t * t) - 1) + b
+    return -c * .5 * ((1 - t * t) ^ .5 - 1) + b
   else
     t = t - 2
-    return c / 2 * (sqrt(1 - t * t) + 1) + b
+    return c * .5 * ((1 - t * t) ^ .5 + 1) + b
   end
 end
 
 local function outInCirc(t, b, c, d)
-  if t < d / 2 then
-    return outCirc(t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outCirc(t * 2, b, c * .5, d)
   else
-    return inCirc((t * 2) - d, b + c / 2, c / 2, d)
+    return inCirc((t * 2) - d, b + c * .5, c * .5, d)
   end
 end
 
@@ -249,14 +249,14 @@ local function inElastic(t, b, c, d, a, p)
 
   if not a or a < abs(c) then
     a = c
-    s = p / 4
+    s = p * .25
   else
-    s = p / (2 * pi) * asin(c/a)
+    s = p / _2pi * asin(c/a)
   end
 
   t = t - 1
 
-  return -(a * pow(2, 10 * t) * sin((t * d - s) * (2 * pi) / p)) + b
+  return -(a * 2 ^ (10 * t) * sin((t * d - s) * _2pi / p)) + b
 end
 
 -- a: amplitud
@@ -274,12 +274,12 @@ local function outElastic(t, b, c, d, a, p)
 
   if not a or a < abs(c) then
     a = c
-    s = p / 4
+    s = p * .25
   else
-    s = p / (2 * pi) * asin(c/a)
+    s = p / _2pi * asin(c/a)
   end
 
-  return a * pow(2, -10 * t) * sin((t * d - s) * (2 * pi) / p) + c + b
+  return a * 2 ^ (-10 * t) * sin((t * d - s) * _2pi / p) + c + b
 end
 
 -- p = period
@@ -298,27 +298,27 @@ local function inOutElastic(t, b, c, d, a, p)
 
   if not a or a < abs(c) then
     a = c
-    s = p / 4
+    s = p * .25
   else
-    s = p / (2 * pi) * asin(c / a)
+    s = p / _2pi * asin(c / a)
   end
 
   if t < 1 then
     t = t - 1
-    return -0.5 * (a * pow(2, 10 * t) * sin((t * d - s) * (2 * pi) / p)) + b
+    return -0.5 * (a * 2 ^ (10 * t) * sin((t * d - s) * _2pi / p)) + b
   else
     t = t - 1
-    return a * pow(2, -10 * t) * sin((t * d - s) * (2 * pi) / p ) * 0.5 + c + b
+    return a * 2 ^ (-10 * t) * sin((t * d - s) * _2pi / p ) * 0.5 + c + b
   end
 end
 
 -- a: amplitud
 -- p: period
 local function outInElastic(t, b, c, d, a, p)
-  if t < d / 2 then
-    return outElastic(t * 2, b, c / 2, d, a, p)
+  if t < d * .5 then
+    return outElastic(t * 2, b, c * .5, d, a, p)
   else
-    return inElastic((t * 2) - d, b + c / 2, c / 2, d, a, p)
+    return inElastic((t * 2) - d, b + c * .5, c * .5, d, a, p)
   end
 end
 
@@ -339,18 +339,18 @@ local function inOutBack(t, b, c, d, s)
   s = s * 1.525
   t = t / d * 2
   if t < 1 then
-    return c / 2 * (t * t * ((s + 1) * t - s)) + b
+    return c * .5 * (t * t * ((s + 1) * t - s)) + b
   else
     t = t - 2
-    return c / 2 * (t * t * ((s + 1) * t + s) + 2) + b
+    return c * .5 * (t * t * ((s + 1) * t + s) + 2) + b
   end
 end
 
 local function outInBack(t, b, c, d, s)
-  if t < d / 2 then
-    return outBack(t * 2, b, c / 2, d, s)
+  if t < d * .5 then
+    return outBack(t * 2, b, c * .5, d, s)
   else
-    return inBack((t * 2) - d, b + c / 2, c / 2, d, s)
+    return inBack((t * 2) - d, b + c * .5, c * .5, d, s)
   end
 end
 
@@ -375,7 +375,7 @@ local function inBounce(t, b, c, d)
 end
 
 local function inOutBounce(t, b, c, d)
-  if t < d / 2 then
+  if t < d * .5 then
     return inBounce(t * 2, 0, c, d) * 0.5 + b
   else
     return outBounce(t * 2 - d, 0, c, d) * 0.5 + c * .5 + b
@@ -383,10 +383,10 @@ local function inOutBounce(t, b, c, d)
 end
 
 local function outInBounce(t, b, c, d)
-  if t < d / 2 then
-    return outBounce(t * 2, b, c / 2, d)
+  if t < d * .5 then
+    return outBounce(t * 2, b, c * .5, d)
   else
-    return inBounce((t * 2) - d, b + c / 2, c / 2, d)
+    return inBounce((t * 2) - d, b + c * .5, c * .5, d)
   end
 end
 
